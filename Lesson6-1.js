c42a15a6c1907a66ae0c8c71fad6e82b349cef40
Joanna Pinto <joanna.pinto@shazam.com>
2015-09-03 14:33:20 +0100

    Lesson 6: Make It Modular - first commit

---
+++ Lesson6-1.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function (directory, extension, callback) {
+	fs.readdir(directory, function (err, files) {
+		if (err) {
+          return callback(err)		
+		}
+		for (i = 0; i < files.length; i += 1) {
+			var currentFile = files[i];
+			if (path.extname(currentFile) === extension) {
+				callback(null, currentFile)
+			}
+		}
+	});
+}
