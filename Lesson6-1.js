259cef323925b475ccbc3f08fd08f27b5d03a45a
Joanna Pinto <joanna.pinto@shazam.com>
2015-09-04 17:46:49 +0100

    Lesson 6: Make It Modular - added second file with require and callback to first

--- Lesson6-1.js
+++ Lesson6-1.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = function (directory, extension, callback) {
 	fs.readdir(directory, function (err, files) {
 		if (err) {
-          return callback(err)		
+          callback(err)		
 		}
 		for (i = 0; i < files.length; i += 1) {
 			var currentFile = files[i];
